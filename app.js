e0c2fd50bdd113db7e705effb268b8e70b64439a
Saim Raza Gondal <sgondal.bscs16seecs@seecs.edu.pk>
2021-03-11 01:27:31 +0500

    basic structure [first commit]

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+
+// Router Imports
+const authRouter = require("./routes/api/authRouter");
+
+// Initialize express app
+const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/auth", authRouter);
+
+module.exports = app;
